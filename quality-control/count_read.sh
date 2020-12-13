abca5ca620473b65f9fbd0585d8903a5dc828b5b
Heru Handika <herubiolog@gmail.com>
2020-12-13 16:35:31 -0600

    Update to using gnu parallel

--- quality-control/count_read.sh
+++ quality-control/count_read.sh
@@ -1,1 +1,3 @@
-for i in sequences/*R1_001.fastq.gz; do echo $i; gunzip -c -f $i | awk 'END {print NR/4}'; done
+#!/bin/bash
+
+parallel "echo {}; zcat {} | awk 'END {print NR/4}';" ::: sequences/*.gz
